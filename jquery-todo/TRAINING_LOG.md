32bad3a84600082e16d5756659644fa5abb7378a
Joshua Briley <josh@thebrileys.com>
2020-09-09 07:04:11 -0400

    JQuery Todo Function refactor

---
+++ jquery-todo/TRAINING_LOG.md
@@ -0,0 +1,47 @@
+# 8/27/20
+
+Today is my first day. I spent 1:20 reviewing the jQuery Todo code example. I also reviewed the ES6 example. Reminder to self:  need to review .bind() and other methods (I completed the prerequisite course months ago and should probably review the whole thing)
+
+# 8/28/20
+
+JQuery Todo:
+
+- watched “The Steps” video
+- Re-reviewed code base
+- Began taking notes on application and prepping questions for next meeting
+
+# 8/31/20
+
+Reading exercises:
+
+- Still in the beginning video series, reading the JQuery Todo code and watching the videos.
+- Completed video 4: Event Listeners, toggle, delete, filter video. I copied the code down to my local machine to run things locally. It is making it easier for me to track and make notes of things.
+- Started on video 5 this morning. Found the commentary around naming methods insightful (specifically the edit method).
+- Was not aware of the ability to set a value as an argument for the `.val()` method, and appreciated the clarification that this was extraneous code. I probably wouldn't have thought to dig that deep.
+
+# 9/1/20
+
+Reading exercises: 
+
+- Unable to attend accountability zoom chat. :-(
+- Completed video 5: edit, editKeyup, update
+- Began Video 6: Rendering with templates
+  - Got through `render()` and `renderFooter()` methods.
+  - Read through Handlebars doc on Github
+
+# 9/2/20
+
+Reading exercises:
+- Finished Video 6: Rendering with templates.
+- I realize this isn't a Handlebars course, but I'm wondering about the benefit of regestering this helper `Handlebars.register('eq', ...)`. Is there a benefit that supercedes the use of a regular conditional for the filter in the footer template?
+  - Tested `<a {{#if filter == 'all'}}class="selected"{{/if}} href="#/all">All</a>` in place of `{{#eq ...}}`. Appears to yield the same results.
+
+# 9/3/20
+
+Reading exercises
+
+- Finished Video 7: localStorage(), init
+  - Got a little familiar with Director.js
+    - [Director.js docs](https://www.github.com/flatiron/director)
+    - Reviewed [routing demo](https://hyperdev.com/#!/project/hurricane=flower)
+- Ready to begin the exercise section!
---
+++ jquery-todo/public/js/app.js
@@ -0,0 +1,218 @@
+/*global jQuery, Handlebars, Router */
+jQuery(function ($) {
+	'use strict';
+
+	Handlebars.registerHelper('eq', function (a, b, options) {
+		return a === b ? options.fn(this) : options.inverse(this);
+	});
+
+	var ENTER_KEY = 13;
+	var ESCAPE_KEY = 27;
+
+	var App = {
+		todos: store('todos-jquery'),
+		todoTemplate: Handlebars.compile($('#todo-template').html()),
+		footerTemplate: Handlebars.compile($('#footer-template').html()),
+		filter: '/all'
+	};
+	
+
+	// UTIL Method/Function extraction
+	function uuid() {
+		/*jshint bitwise:false */
+		var i, random;
+		var uuid = '';
+
+		for (i = 0; i < 32; i++) {
+			random = Math.random() * 16 | 0;
+			if (i === 8 || i === 12 || i === 16 || i === 20) {
+				uuid += '-';
+			}
+			uuid += (i === 12 ? 4 : (i === 16 ? (random & 3 | 8) : random)).toString(16);
+		}
+
+		return uuid;
+	}
+
+	function pluralize(count, word) {
+		return count === 1 ? word : word + 's';
+	}
+
+	function store(namespace, data) {
+		if (arguments.length > 1) {
+			return localStorage.setItem(namespace, JSON.stringify(data));
+		} else {
+			var store = localStorage.getItem(namespace);
+			return (store && JSON.parse(store)) || [];
+		}
+	}
+
+	// APP Method/Function extraction
+
+	function init() {
+		bindEvents();
+		new Router({
+			'/:filter': function (filter) {
+				App.filter = filter;
+				render();
+			}.bind(this)
+		}).init(App.filter);      
+	}
+
+	function bindEvents() {
+		$('#new-todo').on('keyup', create.bind(this));
+		$('#toggle-all').on('change', toggleAll.bind(this));
+		$('#footer').on('click', '#clear-completed', destroyCompleted.bind(this));
+		$('#todo-list')
+			.on('change', '.toggle', toggle.bind(this))
+			.on('dblclick', 'label', edit.bind(this))
+			.on('keyup', '.edit', editKeyup.bind(this))
+			.on('focusout', '.edit', update.bind(this))
+			.on('click', '.destroy', destroy.bind(this));
+	}
+
+	function render() {
+		var todos = getFilteredTodos();
+		$('#todo-list').html(App.todoTemplate(todos));
+		$('#main').toggle(todos.length > 0);
+		$('#toggle-all').prop('checked', getActiveTodos().length === 0);
+		renderFooter();
+		$('#new-todo').focus();
+		store('todos-jquery', App.todos);
+	}
+
+	function update(e) {
+		console.log('update')
+		var el = e.target;
+		var $el = $(el);
+		var val = $el.val().trim();
+
+		if (!val) {
+			this.destroy(e);
+			return;
+		}
+
+		if ($el.data('abort')) {
+			$el.data('abort', false);
+		} else {
+			App.todos[indexFromEl(el)].title = val;
+		}
+
+		render();
+	}
+
+	function editKeyup(e) {
+		console.log('edit key up')
+		if (e.which === ENTER_KEY) {
+			e.target.blur();
+		}
+
+		if (e.which === ESCAPE_KEY) {
+			$(e.target).data('abort', true).blur();
+		}
+	}
+
+	function edit(e) {
+		var $input = $(e.target).closest('li').addClass('editing').find('.edit');
+		$input.val($input.val()).focus();
+	}
+
+	// accepts an element from inside the `.item` div and
+	// returns the corresponding index in the `todos` array
+	function indexFromEl(el) {
+		var id = $(el).closest('li').data('id');
+		var todos = App.todos;
+		var i = todos.length;
+
+		while (i--) {
+			if (todos[i].id === id) {
+				return i;
+			}
+		}
+	}
+
+	function create(e) {
+		var $input = $(e.target);
+		var val = $input.val().trim();
+
+		if (e.which !== ENTER_KEY || !val) {
+			return;
+		}
+
+		App.todos.push({
+			id: uuid(),
+			title: val,
+			completed: false
+		});
+
+		$input.val('');
+
+		render();
+	}
+
+	function destroyCompleted() {
+		App.todos = getActiveTodos();
+		App.filter = 'all';
+		render();
+	}
+
+	function getFilteredTodos() {
+		if (App.filter === 'active') {
+			return getActiveTodos();
+		}
+
+		if (App.filter === 'completed') {
+			return getCompletedTodos();
+		}
+
+		return App.todos;
+	}
+
+	function getCompletedTodos() {
+		return App.todos.filter(function (todo) {
+			return todo.completed;
+		});
+	}
+
+	function getActiveTodos() {
+		return App.todos.filter(function (todo) {
+			return !todo.completed;
+		});
+	}
+
+	function toggleAll(e) {
+		var isChecked = $(e.target).prop('checked');
+
+		App.todos.forEach(function (todo) {
+			todo.completed = isChecked;
+		});
+
+		render();
+	}
+
+	function renderFooter() {
+		var todoCount = App.todos.length;
+		var activeTodoCount = getActiveTodos().length;
+		var template = App.footerTemplate({
+			activeTodoCount: activeTodoCount,
+			activeTodoWord: pluralize(activeTodoCount, 'item'),
+			completedTodos: todoCount - activeTodoCount,
+			filter: App.filter
+		});
+
+		$('#footer').toggle(todoCount > 0).html(template);
+	}
+
+	function toggle(e) {
+		var i = indexFromEl(e.target);
+		App.todos[i].completed = !App.todos[i].completed;
+		render();
+	}
+
+	function destroy(e) {
+		App.todos.splice(indexFromEl(e.target), 1)
+		render()
+	} 
+
+	init();
+});
